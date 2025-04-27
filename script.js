de4dad4bc2c7c2d588c6c7a8fa0d88e400b4d422
ASHFAQUE ALI CHANNA <channaashfaq34@gmail.com>
2025-04-27 23:30:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const audio = document.getElementById('audio');
+const playBtn = document.getElementById('play');
+
+playBtn.addEventListener('click', () => {
+  if (audio.paused) {
+    audio.play();
+    playBtn.textContent = '⏸️';
+  } else {
+    audio.pause();
+    playBtn.textContent = '▶️';
+  }
+});
